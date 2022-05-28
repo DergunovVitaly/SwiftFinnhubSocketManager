a24c8963291c5c216f9aefd7726e46a4c2cb4cd3
Vitalii <dergunov.vitaly@gmail.com>
2022-05-28 09:51:45 +0300

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# SwiftFinnhubSocketManager
+
+A description of this package.
---
+++ Tests/SwiftFinnhubSocketManagerTests/SwiftFinnhubSocketManagerTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import SwiftFinnhubSocketManager
+
+final class SwiftFinnhubSocketManagerTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(SwiftFinnhubSocketManager().text, "Hello, World!")
+    }
+}
